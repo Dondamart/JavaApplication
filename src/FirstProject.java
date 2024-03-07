bfd3923617de9f58d8df1d1d1e960715338949b1
Dondamart <daniyermo@gmail.com>
2024-03-07 12:08:51 +0100

    JAVA1st
    Hello World

--- src/FirstProject.java
+++ src/FirstProject.java
@@ -9,6 +9,6 @@
  */
 public class FirstProject {
     public static void main(String[] args){
-        System.out.println("Hello World");
+        System.out.println("Hello World, this is my first JAVA project");
     }
 }
